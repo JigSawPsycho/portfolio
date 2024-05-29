76aacb30ea7f283597da5bf810df9a625248890c
mikelaptop <micheal@skillsvr.com>
2024-05-29 19:30:29 +1200

    feat: animations to portfolio items

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,20 @@
 // Scripts
 // 
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('.show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
