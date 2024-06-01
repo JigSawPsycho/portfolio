540abb675c6d0c3f020751b74ee1ad41b64b7a50
mikelaptop <micheal@skillsvr.com>
2024-06-01 14:31:50 +1200

    fix: duplicate code in js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,6 @@
 // Scripts
 // 
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('.show');
-        }
-    })
-})
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
